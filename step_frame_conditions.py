4ea7c5cdfadabcc68107a05b61e27ba1be5e3cab
lucianIrsigler <l.irsigler64@gmail.com>
2025-07-07 18:53:01 +0200

    (refactor): Changed function name

---
+++ step_frame_conditions.py
@@ -0,0 +1,57 @@
+from util.formula import initSubformula
+from util.nomial import Nominal
+from util.preprocess import preprocess
+from AST.abstract_syntax_tree import AbstractSyntaxTree
+from AST.abstract_operator import AbstractOperator
+
+operators = [
+    # perf 2
+    AbstractOperator("=>",2,2),
+
+    # perf 1
+    AbstractOperator("<",2,1),
+    AbstractOperator("<'",2,1),
+
+    #perf 0
+    AbstractOperator("#",1),
+    AbstractOperator("#'",1),
+    AbstractOperator("@",1),
+    AbstractOperator("@'",1),
+    AbstractOperator("~",1),
+    AbstractOperator("i*",1),
+    AbstractOperator("i!",1),
+    AbstractOperator("i",1),
+
+    AbstractOperator("^",2),
+    AbstractOperator("&",2),
+    AbstractOperator("->",2),
+    AbstractOperator("<->",2),
+
+
+]
+nominalTracker = Nominal()
+
+# TODO : make operators default so less initialization is needed
+abstractTree = AbstractSyntaxTree(operators)
+
+
+def init_procedure(rule:str)->str:
+    """Constructs the initial formula from the reduced rule
+
+    Args:
+        rule (str): rule to initialize
+
+    Returns:
+        str: initialized rule
+    """
+    output = ""
+    premise,conclusion = rule.split("/")
+    output += initSubformula(premise)
+    output += "=>"
+    output += initSubformula(conclusion)
+    return output
+
+rule = "#x->y/#x->#y"
+formula = init_procedure(rule)
+abstractTree.buildTree(formula)
+
