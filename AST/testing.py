78e436c7adb7859543c35d4fa263d5d86f47a4be
lucianIrsigler <l.irsigler64@gmail.com>
2025-07-08 16:55:46 +0200

    (test): Added dynamic testing+testing per folder - The root level testing.py checks for testing.py in all the folders and runs the suites in them. - Added tests for shunting yard algorihtm, and AST

--- AST/testing.py
+++ AST/testing.py
@@ -1,40 +1,105 @@
-from  abstract_operator import AbstractOperator
-from  abstract_syntax_tree import AbstractSyntaxTree
-
-#TODO UPDATE
-operators = [
-    # perf 2
-    AbstractOperator("=>",2,2),
-
-    # perf 1
-    AbstractOperator("<",2,1),
-    AbstractOperator("<'",2,1),
-
-    #perf 0
-    AbstractOperator("#",1),
-    AbstractOperator("#'",1),
-    AbstractOperator("@",1),
-    AbstractOperator("@'",1),
-    AbstractOperator("~",1),
-    AbstractOperator("i*",1),
-    AbstractOperator("i!",1),
-    AbstractOperator("i",1),
-
-    AbstractOperator("^",2),
-    AbstractOperator("&",2),
-    AbstractOperator("->",2),
-    AbstractOperator("<->",2),
+# add tests for stepFrameConditions.py
+import unittest
+from AST.shunting_yard import shuntingYard,replaceCharacters
+from AST.abstract_syntax_tree import AbstractSyntaxTree
+from AST.AST_error import ASTError
 
 
-]
+class TestShunt(unittest.TestCase):
+    def test_shunting_yard(self):
+        self.assertEqual(shuntingYard(""), "")
 
-# TODO : make operators default so less initialization is needed
-abstractTree = AbstractSyntaxTree(operators)
+        tests = [
+            ("#x<i(y)=>#x<#y", "x # y i < x # y # < =>"),
+            ("i*(#x)<y=>#x<#y", "x # i* y < x # y # < =>"),
+            ("#x<#i*(#x)", "x # x # i* # <"),
+            ("w<#x=>w<#i*(#x)", "w x # < w x # i* # < =>"),
+            ("@'w<x=>w<#i*(#x)", "w @' x < w x # i* # < =>"),
+            ("w<#i*(#@'w)", "w w @' # i* # <"),
+                   # Additional tests:
+            ("i!i(y)", "y i i!"),                    # nested unary operators i! applied twice
+            ("~#x&y", "x # ~ y &"),                   # unary ~ with binary &
+            ("x=>y|z", "x y z | =>"),                  # implication and disjunction
+            ("(x&y)|z", "x y & z |"),                 # parentheses to group
+            ("i!(#@'x)", "x @' # i!"),              # unary i! with binary &
+            ("i*(@z)", "z @ i*"),                    # unary i* with unary @
+            ("i(#@'#@x)<z", "x @ # @' # i z <"),              # unary # with binary |
+            ("i(i!(x))", "x i! i"),                   # nested i operators with parentheses
+            ("i(i!(i*(x)))", "x i* i! i"),                   # nested i operators with parentheses
+            ("w<#x=>w<#i!(#x)", "w x # < w x # i! # < =>"),
+        ]
 
-# abstractTree.buildTree("i(x)=>i(y)")
-# i(x) i(y) => 
-formula = "#x<i(y)=>#x<#y"
-# formula = "#x<y=>w_1^w_2 => #'x"
-abstractTree.buildTree(formula)
+        for formula, expected_postfix in tests:
+            print(formula)
+            reverted = shuntingYard(formula)
+            self.assertEqual(reverted, expected_postfix)
 
-abstractTree.printTree()
+
+class TestAbstractSyntaxTree(unittest.TestCase):
+    def setUp(self):
+        self.ast = AbstractSyntaxTree()
+
+    def test_empty_formula(self):
+        with self.assertRaises(ASTError):
+            self.ast.buildTree("")
+
+    def test_simple_operand(self):
+        root = self.ast.buildTree("x")
+        self.assertEqual(root.value, "x")
+        self.assertEqual(root.arity, 0)
+        self.assertIsNone(getattr(root, 'left', None))
+        self.assertIsNone(getattr(root, 'right', None))
+        self.assertIsNone(getattr(root, 'child', None))
+
+    def test_unary_operator(self):
+        root = self.ast.buildTree("#x")
+        self.assertEqual(root.value, "#")
+        self.assertEqual(root.arity, 1)
+        self.assertIsNotNone(root.child)
+        self.assertEqual(root.child.value, "x")
+        self.assertEqual(root.child.arity, 0)
+
+    def test_binary_operator(self):
+        root = self.ast.buildTree("#x<#y")
+        self.assertEqual(root.value, "<")
+        self.assertEqual(root.arity, 2)
+        self.assertIsNotNone(root.left)
+        self.assertIsNotNone(root.right)
+        self.assertEqual(root.left.value, "#")
+        self.assertEqual(root.right.value, "#")
+        self.assertEqual(root.left.child.value, "x")
+        self.assertEqual(root.right.child.value, "y")
+
+    def test_complex_formula(self):
+        formula = "#x<y=>#x<#y"
+        root = self.ast.buildTree(formula)
+        # root should be =>
+        self.assertEqual(root.value, "=>")
+        self.assertEqual(root.arity, 2)
+        # left subtree is (#x < y)
+        left = root.left
+        self.assertEqual(left.value, "<")
+        self.assertEqual(left.arity, 2)
+        self.assertEqual(left.left.value, "#")
+        self.assertEqual(left.left.arity, 1)
+        self.assertEqual(left.left.child.value, "x")
+        self.assertEqual(left.right.value, "y")
+        # right subtree is (#x < #y)
+        right = root.right
+        self.assertEqual(right.value, "<")
+        self.assertEqual(right.arity, 2)
+        self.assertEqual(right.left.value, "#")
+        self.assertEqual(right.left.child.value, "x")
+        self.assertEqual(right.right.value, "#")
+        self.assertEqual(right.right.child.value, "y")
+
+    def test_invalid_expression_not_enough_operands(self):
+        with self.assertRaises(ASTError):
+            self.ast.buildTree("#")
+
+    def test_invalid_expression_extra_operands(self):
+        with self.assertRaises(ASTError):
+            self.ast.buildTree("x y")
+
+if __name__ == "__main__":
+    unittest.main()
--- util/testing.py
+++ util/testing.py
@@ -1,4 +1,66 @@
-from formula import findAtomicFormulas, getConnectives, checkIfFree, initAtomicFormula, initSubformula
+import unittest
+from util.preprocess import checkIfNoTextAfterCharacter, checkIfNoTextBeforeCharacter, checkIfValidCharacters
+from util.errors import InputError
+import util.formula as formula_util
 
+class UtilTesting(unittest.TestCase):
+    # Preprocessing Tests
+    def test_checkIfNoTextAfterCharacter(self):
+        self.assertTrue(checkIfNoTextAfterCharacter("test#", "#"))
+        self.assertFalse(checkIfNoTextAfterCharacter("test#text", "#"))
+        self.assertTrue(checkIfNoTextAfterCharacter("test", "#"))
 
-print(initSubformula("#x")) # <->
+    def test_checkIfNoTextBeforeCharacter(self):
+        self.assertTrue(checkIfNoTextBeforeCharacter("#test", "#"))
+        self.assertFalse(checkIfNoTextBeforeCharacter("text#test", "#"))
+        self.assertTrue(checkIfNoTextBeforeCharacter("test", "#"))
+
+    def test_checkIfValidCharacters(self):
+        valid_rule = "a#b@c->d<->e"
+        invalid_rule = "a#b@c->d<->e$"
+
+        self.assertTrue(checkIfValidCharacters(valid_rule))
+        with self.assertRaises(InputError):
+            checkIfValidCharacters(invalid_rule)
+
+    # Formula tests
+    def test_checkIfFree(self):
+        self.assertTrue(formula_util.checkIfFree("x"))
+        self.assertFalse(formula_util.checkIfFree("#x"))
+        self.assertFalse(formula_util.checkIfFree("@x"))
+    
+    def test_initAtomicFormula(self):
+        self.assertEqual(formula_util.initAtomicFormula("x"), "i(x)")
+        self.assertEqual(formula_util.initAtomicFormula("#x"), "#x")
+        self.assertEqual(formula_util.initAtomicFormula("@x"), "@x")
+
+    """def test_initSubformula(self):
+        self.assertEqual(formula_util.initSubformula("x"), "i(x)")
+        self.assertEqual(formula_util.initSubformula("#x"), "#x")
+        self.assertEqual(formula_util.initSubformula("@x"), "@x")
+        self.assertEqual(formula_util.initSubformula("x&y"), "i(x)&i(y)")
+        self.assertEqual(formula_util.initSubformula("x=>y"), "i(x)=>i(y)")
+
+    def test_findAtomicFormulas(self):
+        formula = "x&y|z"
+        expected = ["x", "y", "z"]
+        result = formula_util.findAtomicFormulas(formula)
+        self.assertEqual(result, expected)
+    
+    def test_getConnectives(self):
+        self.assertEqual(formula_util.getConnectives("x&y|z"), ["&", "|"])
+        self.assertEqual(formula_util.getConnectives("x=>y"), ["=>"])
+        self.assertEqual(formula_util.getConnectives("x<->y"), ["<->"])
+        self.assertEqual(formula_util.getConnectives("x#y@z"), ["#", "@"])
+        self.assertEqual(formula_util.getConnectives(""),[])
+    
+    def test_getVariable(self):
+        self.assertEqual(formula_util.getVariable("x&y|z"), ["x", "y", "z"])
+        self.assertEqual(formula_util.getVariable("#x@z"), ["x", "z"])
+        self.assertEqual(formula_util.getVariable("i(x)"), ["x"])
+        self.assertEqual(formula_util.getVariable("i(x)&i(y)"), ["x", "y"])
+        self.assertEqual(formula_util.getVariable(""), [])
+        self.assertEqual(formula_util.getVariable("i"), [])
+        self.assertEqual(formula_util.getVariable("#"), [])
+        self.assertEqual(formula_util.getVariable("@'=>@"), [])
+"""
