abe9c0c20e7d9657365853cf780ec869521698da
lucianIrsigler <l.irsigler64@gmail.com>
2025-07-10 19:20:53 +0200

    feat(search): Solution search - Uses greedy best first search to eliminate variables from a formula

---
+++ util/core/solution_search.py
@@ -0,0 +1,66 @@
+from util.core.priority_stack import PriorityStack
+from util.rules.inference_rules import inferenceRules
+import util.rules.ackermann_rules as Ackermann
+
+
+def greedyFirstSearch(formula:str):
+    variables = Ackermann.findVariables(formula)
+    numberVariables = len(variables) 
+    stillSearch = True
+    iterations = 0
+
+    trackState = []
+    trackLog = []
+
+    pq = PriorityStack()
+    pq.push(0,formula)   
+
+    while not pq.empty() and stillSearch and iterations<30:
+        iterations+=1
+        item = pq.pop()
+        trackState.append(item)
+        trackLog.append(f"Current formula:{item}")
+
+        if (item==None):
+            break
+
+        currentVariables = Ackermann.findVariables(item)
+        appliedAck = False
+
+        # Goal test
+        if len(currentVariables)==0:
+            stillSearch = False
+            trackLog.append(f"Goal found:{item}")
+            continue
+        
+        #Check ackermann rule
+        for var in variables:
+            newForm = Ackermann.applyAckermannRule(item)
+
+            if (newForm!= item):
+                trackLog.append(f"Applying Ackermann rule to {item}, yielding {newForm}")
+                pq.push(5,newForm)
+                appliedAck = True
+                break
+        
+        if appliedAck:
+            continue
+        
+        # Do inference rules now
+        currentInferenceRules = inferenceRules(item)
+
+        for subform in currentInferenceRules.keys():
+            for replacement in currentInferenceRules[subform]:
+                tempFormula = item
+                tempFormula = tempFormula.replace(subform,replacement)
+
+                if (tempFormula.count("=>"))>1:
+                    continue
+
+                score = Ackermann.ackermannHeuristic(tempFormula,numberVariables)
+                # print(f"ADDED {tempFormula} with prio {score}")
+                trackLog.append(f"Added potential formula {tempFormula} with priority {score}")
+                pq.push(score,tempFormula)
+
+    return trackState,trackLog
+
