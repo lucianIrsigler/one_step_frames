b95781aae0ca0bac3aceb8d0b63b3034673af5b7
lucianIrsigler <l.irsigler64@gmail.com>
2025-07-30 18:48:19 +0200

    refactor(Moved spass39): moved the files

--- testing.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-if __name__ == '__main__':
-    # Discover all tests named testing.py recursively from current directory
-    suite = unittest.defaultTestLoader.discover(start_dir='.', pattern='testing.py')
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
