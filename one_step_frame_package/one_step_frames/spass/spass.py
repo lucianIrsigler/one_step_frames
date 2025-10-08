2ea67248c66aafce3741053b760d909578ddd5ab
lucianIrsigler <l.irsigler64@gmail.com>
2025-10-08 22:28:56 +0200

    refactor(SPASS): SPASS returns output instead of a file
    SPASS now writes to a unique dfg file, and instead of just writing to a file, it returns the output from SPASS and any errors that occured.

--- one_step_frame_package/one_step_frames/spass/spass.py
+++ one_step_frame_package/one_step_frames/spass/spass.py
@@ -2,5 +2,6 @@ from .util import load_config,create_dfg_file,run_SPASS
 
 def SPASS(first_order:str,S:str,config=None|dict[str,str]):
     config = load_config()
-    create_dfg_file(config,first_order,S)
-    run_SPASS()
+    id = create_dfg_file(config,first_order,S)
+    output,error = run_SPASS(id)
+    return output,error
